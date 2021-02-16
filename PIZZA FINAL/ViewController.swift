5427cfb2568833cdf55bf697deca409d91384da1
Nicolas Russi <NicolasRussi@MacBook-Pro-de-Daniel.local>
2021-02-16 08:24:53 -0500

    Initial Commit

---
+++ PIZZA FINAL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PIZZA FINAL
+//
+//  Created by Nicolas Russi on 16/02/21.
+//  Copyright © 2021 Nicolas Russi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
